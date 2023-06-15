5c64f7c9039093bb3c4139aa1d38d1000d9380e5
alx00ho <46589739+alx00ho@users.noreply.github.com>
2023-06-15 00:16:03 -0400

    Chrome extension finished

--- main.js
+++ main.js
@@ -1,8 +1,52 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const tabBtn = document.getElementById("tab-btn");
+const deleteBtn = document.getElementById("delete-btn");
+const ulEl = document.getElementById("ul-el");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  renderLeads();
+  console.log(localStorage.getItem("myLeads"));
 });
+
+tabBtn.addEventListener("click", () => {
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      myLeads.push(tabs[0].url);
+      localStorage.setItem("myLeads", JSON.stringify(myLeads));
+      renderLeads();
+    }
+  );
+});
+
+deleteBtn.addEventListener("dblclick", () => {
+  myLeads = [];
+  localStorage.clear();
+  renderLeads();
+});
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+console.log(leadsFromLocalStorage);
+
+function renderLeads() {
+  ulEl.innerHTML = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += `
+        <li>
+            <a href="${myLeads[i]}" target=_blank">${myLeads[i]}</a>
+        </li>
+    `;
+  }
+}
